e407af776fbf91bd47a46f4aa061c0f06ee73c71
Aadil Mehraj <aadilmehraj76@gmail.com>
2018-12-27 15:08:12 +0530

    Add OkHttp Logging Interceptor

--- app/src/main/java/com/example/alexr/ideamanager/services/ServiceBuilder.java
+++ app/src/main/java/com/example/alexr/ideamanager/services/ServiceBuilder.java
@@ -1,5 +1,8 @@
 package com.example.alexr.ideamanager.services;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
 import retrofit2.Retrofit;
 import retrofit2.Retrofit.Builder;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -7,9 +10,22 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ServiceBuilder {
 
     private static final String BASE_URL = "http://10.0.2.2:9000";
+
+    // Create Logger
+    private static HttpLoggingInterceptor mLogger = new HttpLoggingInterceptor()
+        .setLevel(Level.BODY);
+
+    // OkHttp Client
+    private static OkHttpClient mOkHttpClient = new OkHttpClient.Builder()
+        .addInterceptor(mLogger)
+        .build();
+
+
     private static Retrofit.Builder mBuilder = new Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create());
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(mOkHttpClient);
+
     private static Retrofit mRetrofit = mBuilder.build();
 
     public static <S> S buildService(Class<S> serviceType) {
